5513fe96cddef843819e430a471e6ab93377fe5c
Brian Vercelli <brianv@sitesusa.local>
2022-05-16 23:09:14 -0700

    initial slack bot setup and test

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+const express = require("express")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+const token = process.env.BOT_TOKEN
+const eventsApi = require('@slack/events-api')
+const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET)
+const { WebClient, LogLevel } = require("@slack/web-api");
+const client = new WebClient(token, {
+  logLevel: LogLevel.DEBUG
+});
+app.use('/', slackEvents.expressMiddleware())
+
+slackEvents.on("message", async (event) => {
+  if (!event.subtype && !event.bot_id) {
+    client.chat.postMessage({
+      token,
+      channel: event.channel,
+      thread_ts: event.ts,
+      text: "Hello World!"
+    })
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+})
